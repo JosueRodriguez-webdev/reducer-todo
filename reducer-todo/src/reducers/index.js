25b1530aa5c97b569aefa8d802e937a9166bc9ce
Josue Rodriguez <jr2589213@gmail.com>
2020-02-17 15:31:01 -0800

    Initialize repo and create component and reducer folder
    Initial commit - began to fill in reducer function and initial state.

---
+++ reducer-todo/src/reducers/index.js
@@ -0,0 +1,14 @@
+export const initialState = {
+    note: 'Learn about reducers',
+    noteToggle: false
+}
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+
+        case inputHere:
+
+    default:
+        return state
+    }
+}
